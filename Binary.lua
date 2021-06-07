e00e486248f6e082a82538f26acb57cdfa083d60
Jeffrey <slassarg20@gmail.com>
2021-06-06 23:53:59 -0400

    Validations Added

--- Binary.lua
+++ Binary.lua
@@ -1,16 +1,29 @@
-print("This is the binary Program")
+print("Binary Program")
 
+print("Please insert the number")
+input1 = io.read()
 
-number = 568 
-base = 10 
+print("Please insert the base")
+input2 = io.read()
+
+number = tonumber(input1)
+base = tonumber(input2)
 result = ""
 
+if(base == 2)
+then 
+    text = "The binary number of " .. number .. " is: "
+else
+    if(base == 10)
+    then 
+        text = "The decial number of " .. number .. " is: "
+    end
+end
 
-while number > 0
+while number > 0 
 do 
-    result = "" .. (number % base) .. result 
+    result = "" .. (number % base) .. result
     number = math.floor(number / base)
-    print(number)
-end 
+end
 
-print(result)
+print(text, result)
